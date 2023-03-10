170ed02071f55def1107f51b870e02a1744c6798
Salvador Gonzalez <alejandroexodia@gmail.com>
2023-03-09 19:13:19 -0600

    practica numero 6 GM190689

---
+++ MyApplicationPractica06DSM104/app/src/main/java/com/example/myapplicationpractica06_dsm104/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapplicationpractica06_dsm104
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickFrame(v: View?){
+        val llamar = Intent(this, FrameLayout::class.java)
+        startActivity(llamar)
+    }
+
+    fun onClickLinear(v: View?){
+        val llamar = Intent(this, LinearLayout::class.java)
+        startActivity(llamar)
+    }
+
+    fun onClickRelative(v: View?){
+        val llamar = Intent(this, RelativeLayout::class.java)
+        startActivity(llamar)
+    }
+
+    fun onClickTable(v: View?){
+        val llamar = Intent(this, TableLayout::class.java)
+        startActivity(llamar)
+    }
+
+    fun onClickGrid(v: View?){
+        val llamar = Intent(this, GridLayout::class.java)
+        startActivity(llamar)
+    }
+}
